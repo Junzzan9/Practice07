f685636dd85725b54bde10f716b00bb27814d037
junzzan9 <ojs2681@gmail.com>
2021-06-11 17:19:01 +0900

    0611

--- src/com/javaex/ex02/Duck.java
+++ src/com/javaex/ex02/Duck.java
@@ -1,34 +1,26 @@
 package com.javaex.ex02;
 
 public class Duck extends Bird{
-	public String name;
+	//private String name;
 	
 	public Duck() {
 		
 	}
 	
 	public Duck(String name) {
-		this.name=name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+		super(name);
 	}
 	
 	public void sing() {
-		System.out.println("오리("+name+")가 날지 않습니다.");
+		System.out.println("오리("+super.getName()+")가 날지 않습니다.");
 	}
 	
 	public void fly() {
-		System.out.println("오리("+name+")가 소리내어 웁니다.");
+		System.out.println("오리("+super.getName()+")가 소리내어 웁니다.");
 	}
 	
 	public void showName() {
-		System.out.println("오리의 이름은"+name+" 입니다.");
+		System.out.println("오리의 이름은"+super.getName()+" 입니다.");
 	}
 	
 	
